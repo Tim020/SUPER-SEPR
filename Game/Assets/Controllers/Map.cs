b64e9599a994889d3578ecd837be766e9abdb965
Matthew Turton Parry <mtpsgame@gmail.com>
2016-11-15 18:03:15 +0000

    Rewrote Map

---
+++ Game/Assets/Controllers/Map.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+    public Sprite grassSprite;
+    public int Width { private set; get; }
+    public int Height { private set; get; }
+
+    void Start () {
+        Width = 32;
+        Height = 32;
+
+        for (int x = 0; x < Width; x++) {
+            for (int y = 0; y < Height; y++) {
+                GameObject go = new GameObject();
+                go.name = "Tile_" + x + "_" + y;
+                go.AddComponent<SpriteRenderer>().sprite = grassSprite;
+                go.transform.position = new Vector3(x, y, 0);
+            }
+        }
+	}
+	
+	void Update () {
+	
+	}
+}
