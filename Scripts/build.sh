0fef5d077cf0e9a239e17ff2a66620ba3b457cd6
Tim020 <timbradgate@hotmail.co.uk>
2016-11-16 19:59:16 +0000

    Continued testing

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,7 +4,7 @@ echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
+  #-silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd)/Game\
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
@@ -15,7 +15,7 @@ echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
+  #-silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd)/Game\
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
@@ -25,7 +25,7 @@ echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
+  #-silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd)/Game\
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
