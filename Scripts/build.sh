29163ec7c9b580af5d0a31fb00725999f43fd71c
Tim020 <timbradgate@hotmail.co.uk>
2016-11-16 19:07:22 +0000

    Try fix travis build error

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,7 +6,7 @@ echo "Attempting to build $project for Windows"
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
+  -projectPath $(pwd) \Game
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
@@ -16,7 +16,7 @@ echo "Attempting to build $project for OS X"
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
+  -projectPath $(pwd) \Game
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
@@ -26,7 +26,7 @@ echo "Attempting to build $project for Linux"
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
+  -projectPath $(pwd) \Game
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
 
