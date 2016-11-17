69e14fcbf8418ac3fac7a12041f8c2064f3bc623
Tim020 <timbradgate@hotmail.co.uk>
2016-11-17 11:54:01 +0000

    I broke something... dammit

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,13 +1,23 @@
-project="SUPER-SEPR-Game"
+project="seprated-Game"
 
-echo "Attempting to build $project for Windows"
+echo "Attempting to build $project for Windows x64"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
+  -silent-crashes \
   -logFile $(pwd)/unity.log \
-  -projectPath $(pwd)/Game\
-  -buildWindowsPlayer "$(pwd)/Build/windows/$(project)_x32.exe" \
-  -buildWindows64Player "$(pwd)/Build/windows64/$(project)_x64.exe" \
+  -projectPath $(pwd) \
+  -buildWindows64Player "$(pwd)/Build/windows64/$project.exe" \
+  -quit
+  
+echo "Attempting to build $project for Windows x32"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows32/$project.exe" \
   -quit
   
 echo 'Logs from build'
