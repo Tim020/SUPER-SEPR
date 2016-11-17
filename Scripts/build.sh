d05911d3345748a80fc599a7a0134b09e6f6aff7
Tim020 <timbradgate@hotmail.co.uk>
2016-11-17 11:41:53 +0000

    Remove Mac builds because they are being weird

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,28 +1,13 @@
 project="SUPER-SEPR-Game"
 
-echo "Working directory for Windows builds"
-pwd
-
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd)/Game\
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -buildWindows64Player "$(pwd)/Build/windows64/$project.exe" \
-  -quit
-  
-echo "Working directory for Mac builds"
-pwd
-
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd)/Game\
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$(project)_x32.exe" \
+  -buildWindows64Player "$(pwd)/Build/windows64/$(project)_x64.exe" \
   -quit
   
 echo 'Logs from build'
