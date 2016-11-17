afd99a79e64ab09b2b89020824cf0961cd95d7b8
Tim020 <timbradgate@hotmail.co.uk>
2016-11-17 21:11:21 +0000

    Fix incorrect generic type in dictionary instantiation

--- Game/Assets/Scripts/Players/Player.cs
+++ Game/Assets/Scripts/Players/Player.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
 	// A dictionary with a resource type as a key, and a tile resource as a value, value is the amount this player currently has
 	private Dictionary<Tile.ResourceType, float> resourceInventory;
@@ -10,22 +11,25 @@ public class Player : MonoBehaviour {
 	private List<Tile> ownedTiles;
 
 	// Called when the game starts
-	protected virtual void Start() {
-		resourceInventory = new Dictionary<Tile.ResourceType, Tile.TileResource>();
-		resourceInventory.Add(Tile.ResourceType.ENERGY, 0);
-		resourceInventory.Add(Tile.ResourceType.ORE, 0);
-		ownedTiles = new List<Tile>();
+	protected virtual void Start ()
+	{
+		resourceInventory = new Dictionary<Tile.ResourceType, float> ();
+		resourceInventory.Add (Tile.ResourceType.ENERGY, 0);
+		resourceInventory.Add (Tile.ResourceType.ORE, 0);
+		ownedTiles = new List<Tile> ();
 	}
-	
-	protected virtual void Update() {
+
+	protected virtual void Update ()
+	{
 	
 	}
 
 	// Iterates through the list of tiles the player owns and gathers the resources it has generated
-	protected virtual void Production() {
+	protected virtual void Production ()
+	{
 		foreach (Tile t in ownedTiles) {
-			resourceInventory[Tile.ResourceType.ENERGY] += t.getResourceAmount(Tile.ResourceType.ENERGY);
-			resourceInventory[Tile.ResourceType.ORE] += t.getResourceAmount(Tile.ResourceType.ORE);
+			resourceInventory [Tile.ResourceType.ENERGY] += t.getResourceAmount (Tile.ResourceType.ENERGY);
+			resourceInventory [Tile.ResourceType.ORE] += t.getResourceAmount (Tile.ResourceType.ORE);
 		}
 	}
 }
