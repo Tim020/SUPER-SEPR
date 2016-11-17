1555b453eabeb6f77b49782ef22b856c94b0c53b
Matthew Turton Parry <mtpsgame@gmail.com>
2016-11-17 11:56:27 +0000

    Added some classes for later

---
+++ Game/Assets/Scripts/Players/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	protected virtual void Start () {
+	
+	}
+	
+	protected virtual void Update () {
+	
+	}
+}
