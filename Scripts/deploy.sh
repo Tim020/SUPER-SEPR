d58855210037b78e8c4ac7f5c454e022ad0aea67
Tim020 <timbradgate@hotmail.co.uk>
2016-11-18 00:31:53 +0000

    Small changes to deploy script

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -2,18 +2,14 @@ project="seprated-Game"
 
 package_build(){
 	build_dir=$1
+	echo "$(pwd)/Build/$build_dir"
 	if [ -d "$(pwd)/Build/$build_dir" ]; then
 		echo "Zipping $build_dir to $build_dir.zip"
 		zip -r "$(pwd)/Build/$build_dir.zip" "$(pwd)/Build/$build_dir"
 	fi
 }
 
-echo 'Navigating to project directory'
-cd "$(pwd)/Build"
-
-package_build "linux"
-package_build "osx"
-package_build "windows32"
-package_build "windows64"
-
-ls -R
+package_build "$(pwd)/Build/linux"
+package_build "$(pwd)/Build/osx"
+package_build "$(pwd)/Build/windows32"
+package_build "$(pwd)/Build/windows64"
