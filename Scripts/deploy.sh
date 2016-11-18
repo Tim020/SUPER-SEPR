7af29e19940cc4d418b0cf290a32932856da089f
Tim020 <timbradgate@hotmail.co.uk>
2016-11-18 10:27:36 +0000

    First attempt at uploading to FTP site

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,4 +1,4 @@
-project="seprated-Game"
+HOST = "mavenrepo.uoy-sepr.smithsmodding.com"
 
 package_build(){
 	build_dir=$1
@@ -9,11 +9,22 @@ package_build(){
 	fi
 	if [ -d "$(pwd)/Build/$build_dir" ]; then
 		echo "Zipping $build_dir to $build_dir.zip"
-		zip -r "$(pwd)/Archive/$build_dir.zip" "$(pwd)/Build/$build_dir"
+		zip -r "$(pwd)/Archive/$TRAVIS_BRANCH-$build_dir-$TRAVIS_BUILD_NUMBER.zip" "$(pwd)/Build/$build_dir"
 	fi
 }
 
-echo "Build number: $TRAVIS_BUILD_NUMBER"
+upload_archive(){
+	dir = $1
+	package_build "$dir"
+	
+	echo "Uploading build $dir to remote repository"
+	ftp -n "$HOST" <<END_SCRIPT
+quote USER "$FTP_USER"
+quote PASS "$FTP_PASSWORD"
+put "$(pwd)/Archive/$TRAVIS_BRANCH-$dir-$TRAVIS_BUILD_NUMBER.zip"
+quit
+END_SCRIPT
+}
 
 package_build "linux"
 package_build "osx"
