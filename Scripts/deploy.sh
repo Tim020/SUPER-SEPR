459237c0ce3961bcc3cef06db220a7a8e0ba7355
Tim020 <timbradgate@hotmail.co.uk>
2016-11-18 09:45:01 +0000

    Create archive folder and try print build number from environment variables

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -3,11 +3,16 @@ project="seprated-Game"
 package_build(){
 	build_dir=$1
 	echo "$(pwd)/Build/$build_dir"
+	if [ ! -d "$(pwd)/Archive"]; then
+		"$(mkdir) -p $(pwd)/Archive"
+	fi
 	if [ -d "$(pwd)/Build/$build_dir" ]; then
 		echo "Zipping $build_dir to $build_dir.zip"
 		zip -r "$(pwd)/Archive/$build_dir.zip" "$(pwd)/Build/$build_dir"
 	fi
 }
+
+echo "Build number: $TRAVIS_BUILD_NUMBER"
 
 package_build "linux"
 package_build "osx"
