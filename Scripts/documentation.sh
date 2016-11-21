f33e9ab1281550ac6007ad3256f2d253807584cc
Tim020 <timbradgate@hotmail.co.uk>
2016-11-21 22:16:05 +0000

    Try add authentication to clone command

--- Scripts/documentation.sh
+++ Scripts/documentation.sh
@@ -19,7 +19,7 @@ mkdir code_docs
 cd code_docs
 
 # Get the current gh-pages branch
-git clone -b gh-pages https://git@$GH_REPO_REF
+git clone -b gh-pages https://$GH_REPO_TOKEN@$GH_REPO_REF
 cd $GH_REPO_NAME
 
 ##### Configure git.
@@ -68,7 +68,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    git push --force "https://$GH_REPO_TOKEN@$GH_REPO_REF" > /dev/null 2>&1
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
