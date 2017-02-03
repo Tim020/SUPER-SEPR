6fe18e755889c4181af4c531e3c0b246f2ac702f
Tim Bradgate <tjb545@its.york.ac.uk>
2017-02-03 12:06:40 +0000

    Adding documentation for the market

--- Game/Assets/Classes/Agents/Market/Market.cs
+++ Game/Assets/Classes/Agents/Market/Market.cs
@@ -4,53 +4,113 @@ using System;
 
 public class Market : Agent {
 
+	/// <summary>
+	/// The casino instance.
+	/// </summary>
 	private Casino casino;
+
+	/// <summary>
+	/// The price the market is selling resources at - used when a player buys from the market.
+	/// </summary>
 	private ResourceGroup resourceSellingPrices;
+
+	/// <summary>
+	/// The price the market is buying resources at - used when a player sells to the market.
+	/// </summary>
 	private ResourceGroup resourceBuyingPrices;
+
+	/// <summary>
+	/// The number roboticons the market has available for sale.
+	/// </summary>
 	private int numRoboticonsForSale;
+
+	/// <summary>
+	/// The price the market is selling roboticons for - used when a player buys a roboticon.
+	/// </summary>
 	private int roboticonBuyingPrice = 15;
 
 	#region Market Starting Constants
 
+	/// <summary>
+	/// The amount of food the market starts with.
+	/// </summary>
 	private const int STARTING_FOOD_AMOUNT = 16;
+
+	/// <summary>
+	/// The amount of energy the market starts with.
+	/// </summary>
 	private const int STARTING_ENERGY_AMOUNT = 16;
+
+	/// <summary>
+	/// The amount of ore the market starts with.
+	/// </summary>
 	private const int STARTING_ORE_AMOUNT = 0;
+
+	/// <summary>
+	/// The amount of roboticons the market starts with.
+	/// </summary>
 	private const int STARTING_ROBOTICON_AMOUNT = 12;
 
+	/// <summary>
+	/// The intial buying price for food.
+	/// </summary>
 	private const int STARTING_FOOD_BUY_PRICE = 10;
+
+	/// <summary>
+	/// The initial buying price for ore.
+	/// </summary>
 	private const int STARTING_ORE_BUY_PRICE = 10;
+
+	/// <summary>
+	/// The initial buyinh price for energy.
+	/// </summary>
 	private const int STARTING_ENERGY_BUY_PRICE = 10;
+
+	/// <summary>
+	/// The initial selling price for food.
+	/// </summary>
 	private const int STARTING_FOOD_SELL_PRICE = 10;
+
+	/// <summary>
+	/// The initial selling price for ore.
+	/// </summary>
 	private const int STARTING_ORE_SELL_PRICE = 10;
+
+	/// <summary>
+	/// The initial selling price for energy.
+	/// </summary>
 	private const int STARTING_ENERGY_SELL_PRICE = 10;
 
+	/// <summary>
+	/// The amount of money the market starts with.
+	/// </summary>
 	private const int STARTING_MONEY = 100;
 
 	#endregion
 
+	/// <summary>
+	/// The amount of ore required for producing more roboticons.
+	/// </summary>
 	private const int ROBOTICON_PRODUCTION_COST = 12;
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Market"/> class.
+	/// </summary>
 	public Market() {
-		resourceSellingPrices = new ResourceGroup(STARTING_FOOD_BUY_PRICE, STARTING_ENERGY_BUY_PRICE,
-			STARTING_ORE_BUY_PRICE);
-		resourceBuyingPrices = new ResourceGroup(STARTING_FOOD_SELL_PRICE, STARTING_ENERGY_SELL_PRICE,
-			STARTING_ORE_SELL_PRICE);
+		resourceSellingPrices = new ResourceGroup(STARTING_FOOD_BUY_PRICE, STARTING_ENERGY_BUY_PRICE, STARTING_ORE_BUY_PRICE);
+		resourceBuyingPrices = new ResourceGroup(STARTING_FOOD_SELL_PRICE, STARTING_ENERGY_SELL_PRICE, STARTING_ORE_SELL_PRICE);
 		resources = new ResourceGroup(STARTING_FOOD_AMOUNT, STARTING_ENERGY_AMOUNT, STARTING_ORE_AMOUNT);
 		numRoboticonsForSale = STARTING_ROBOTICON_AMOUNT;
 		money = STARTING_MONEY;
 	}
 
 	/// <summary>
-	/// Throws System.ArgumentException if the market does not have enough resources 
-	/// to complete the transaction.
+	/// Buy resources from the market.
 	/// </summary>
-	/// <param name="resources"></param>
-	/// <param name="price"></param>
+	/// <param name="resourcesToBuy">The resources the player is wishing to buy</param>
+	/// <exception cref="System.ArgumentException">When the market does not have enough resources to complete the transaction</exception>
 	public void BuyFrom(ResourceGroup resourcesToBuy) {
-		bool hasEnoughResources = !(resourcesToBuy.food > resources.food
-		                          || resourcesToBuy.energy > resources.energy
-		                          || resourcesToBuy.ore > resources.ore);
-
+		bool hasEnoughResources = !(resourcesToBuy.food > resources.food || resourcesToBuy.energy > resources.energy || resourcesToBuy.ore > resources.ore);
 		if (hasEnoughResources) {
 			//Requires subtraction overload
 			resources -= resourcesToBuy;
@@ -62,11 +122,10 @@ public class Market : Agent {
 	}
 
 	/// <summary>
-	/// Throws System.ArgumentException if the market does not have enough money
-	/// to complete the transaction.
+	/// Sell resources to the market.
 	/// </summary>
-	/// <param name="resource"></param>
-	/// <param name="price"></param>
+	/// <param name="resourcesToSell">The resources the player wishes to sell to the market</param>
+	/// <exception cref="System.ArgumentException">When the market does not have enough money to complete the transaction.</exception>
 	public void SellTo(ResourceGroup resourcesToSell) {
 		if (resourcesToSell.getFood() < 0 || resourcesToSell.getEnergy() < 0 || resourcesToSell.getOre() < 0) {
 			throw new ArgumentException("Market cannot complete a transaction for negative resources.");
@@ -83,10 +142,16 @@ public class Market : Agent {
 		}
 	}
 
+	/// <summary>
+	/// Updates the prices for resources based on supply and demand economics.
+	/// TODO: Implement this
+	/// </summary>
 	public void UpdatePrices() {
-		//Skeleton for later use when adding supply & demand
 	}
 
+	/// <summary>
+	/// Produces the roboticon if enough resources are available.
+	/// </summary>
 	public void ProduceRoboticon() {
 		if (resources.ore >= ROBOTICON_PRODUCTION_COST) {
 			resources.ore -= ROBOTICON_PRODUCTION_COST;
