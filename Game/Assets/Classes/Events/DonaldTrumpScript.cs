b94b43e370c0aa5f286356b0d2e00e69cbfb6d56
Matthew Turton Parry <mjtp501@its.york.ac.uk>
2017-02-14 12:25:31 +0000

    Fixed the donald trump event

---
+++ Game/Assets/Classes/Events/DonaldTrumpScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonaldTrumpScript : MonoBehaviour {
+
+    public void StartEvent() {
+        Invoke("HideSprite", 3f);
+    }
+
+	private void HideSprite() {
+        gameObject.SetActive(false);
+    }
+}
