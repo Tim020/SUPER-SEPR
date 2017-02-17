60081a63723247e17d0428c6974e3e7760fd5a36
Tim Bradgate <tjb545@its.york.ac.uk>
2017-02-17 16:30:04 +0000

    Implemented gambling

--- Game/Assets/Classes/Agents/Market/Casino.cs
+++ Game/Assets/Classes/Agents/Market/Casino.cs
@@ -1,40 +1,51 @@
 ﻿// Game Executable hosted at: http://www-users.york.ac.uk/~jwa509/alpha01BugFree.exe
-
 using UnityEngine;
+using System;
 
 /// <summary>
-/// The casino used within the market to gamble with
+/// The casino used to gamble with
 /// </summary>
 public class Casino : MonoBehaviour {
 
 	/// <summary>
-	/// The fairness - how likely you are to win (higher is less likely).
+	/// The max win percentage.
 	/// </summary>
-	private int fairness;
+	private int maxWinPercentage;
+
+	/// <summary>
+	/// Percentage of the time you win the gamble.
+	/// </summary>
+	public int minRollNeeded{ private set; get; }
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="Casino"/> class.
 	/// </summary>
-	/// <param name="fairness">The fairness of the casino.</param>
-	public Casino(int fairness) {
-		this.fairness = Mathf.Clamp(fairness, 0, 100);
+	/// <param name="minWinRoll">The maximum value for the minimum roll needed to win that can be generated.</param>
+	/// <example>Setting the parameter to 35 will randomly generate the number needed to win up to that. In effect the maximum chance you have to win is 35</example>
+	public Casino(int maxWinPercentage) {
+		this.maxWinPercentage = maxWinPercentage;
+		this.minRollNeeded = 100 - UnityEngine.Random.Range(5, this.maxWinPercentage + 1);
 	}
 
 	/// <summary>
 	/// Gambles the money.
+	/// Generates a random win amount each time also, up to the max value set.
 	/// </summary>
-	/// <returns>The net result of the gambling.</returns>
+	/// <param name="player">The player gambling.</param>
 	/// <param name="gambleAmount">The amount of money to gamble with.</param>
-	public int GambleMoney(int gambleAmount) {
-		//Losing is default
-		int netAmount = -gambleAmount;
-		int roll = Random.Range(0, 101);
-
-		if (roll < fairness) {
-			// First gambleAmount makes netAmount back to zero
-			netAmount = gambleAmount + 2 * gambleAmount;
+	public Data.Tuple<int, bool> GambleMoney(AbstractPlayer player, int gambleAmount) {
+		if (player.GetMoney() >= gambleAmount) {
+			int roll = UnityEngine.Random.Range(0, 101);
+			if (roll >= minRollNeeded) {
+				player.GiveMoney(2 * gambleAmount);
+			} else {
+				player.DeductMoney(gambleAmount);
+			}
+			this.minRollNeeded = 100 - UnityEngine.Random.Range(5, this.maxWinPercentage + 1);
+			return new Data.Tuple<int, bool>(roll, roll >= minRollNeeded);
+		} else {
+			throw new ArgumentException("The gamble is invalid as the player does not have enough money!");
+			return new Data.Tuple<int, bool>(0, false);
 		}
-		return netAmount;
 	}
-
 }
