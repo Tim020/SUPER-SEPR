c7274f933da25b46371cdd8213f41f230f10f070
Tim Bradgate <tjb545@its.york.ac.uk>
2017-02-17 11:10:25 +0000

    Clamp the fairness between the roll amounts

--- Game/Assets/Classes/Agents/Market/Casino.cs
+++ Game/Assets/Classes/Agents/Market/Casino.cs
@@ -10,31 +10,31 @@ public class Casino : MonoBehaviour {
 	/// <summary>
 	/// The fairness - how likely you are to win (higher is less likely).
 	/// </summary>
-    private int fairness;
+	private int fairness;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="Casino"/> class.
 	/// </summary>
 	/// <param name="fairness">The fairness of the casino.</param>
-    public Casino(int fairness) {
-        this.fairness = fairness;
-    }
+	public Casino(int fairness) {
+		this.fairness = Mathf.Clamp(fairness, 0, 100);
+	}
 
 	/// <summary>
 	/// Gambles the money.
 	/// </summary>
 	/// <returns>The net result of the gambling.</returns>
 	/// <param name="gambleAmount">The amount of money to gamble with.</param>
-    public int GambleMoney(int gambleAmount) {
-        //Losing is default
-        int netAmount = -gambleAmount;
-        int roll = Random.Range(0, 101);
+	public int GambleMoney(int gambleAmount) {
+		//Losing is default
+		int netAmount = -gambleAmount;
+		int roll = Random.Range(0, 101);
 
-        if (roll < fairness) {
-            // First gambleAmount makes netAmount back to zero
-            netAmount = gambleAmount + 2 * gambleAmount;
-        }
-        return netAmount;
-    }
+		if (roll < fairness) {
+			// First gambleAmount makes netAmount back to zero
+			netAmount = gambleAmount + 2 * gambleAmount;
+		}
+		return netAmount;
+	}
 
 }
